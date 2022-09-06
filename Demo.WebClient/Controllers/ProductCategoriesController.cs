1b275864f1788b563177656ce03a4837bcb30ab7
Vojtech Komurka <ramok@centrum.cz>
2022-09-06 23:27:38 +0200

    Add base list views.

---
+++ Demo.WebClient/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.WebClient.Controllers
+{
+    public class ProductCategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Demo.WebClient/Controllers/WarehousesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.WebClient.Controllers
+{
+    public class WarehousesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
