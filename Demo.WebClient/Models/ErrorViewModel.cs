f5294e1a860a8e8fd327cfaa98cefc2a92a08ed3
Vojtech Komurka <ramok@centrum.cz>
2022-09-07 16:30:47 +0200

    Remove MVC web client.

--- Demo.WebClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Demo.WebClient.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
