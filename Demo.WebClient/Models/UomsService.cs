9320d8dcef2e8058221abdd63f6604207832200e
Vojtech Komurka <ramok@centrum.cz>
2022-09-07 16:23:14 +0200

    Add MVC test client.

---
+++ Demo.WebClient/Models/UomsService.cs
@@ -0,0 +1,13 @@
+﻿using Demo.Contracts.Dtos;
+
+namespace Demo.WebClient.Models
+{
+    public static class UomsService
+    {
+        public static List<UomDto> Uoms { get; set; } = new()
+        {
+            new UomDto() { Id = Guid.NewGuid(), Code = "kg" },
+            new UomDto() { Id = Guid.NewGuid(), Code = "pcs" },
+        };
+    }
+}
