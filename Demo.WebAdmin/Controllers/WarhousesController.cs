8abd3d05f555f1344fc97114973a31138e2864eb
Vojtech Komurka <ramok@centrum.cz>
2022-09-09 14:18:11 +0200

    Remove ClientServices and WebClient projects.

---
+++ Demo.WebAdmin/Controllers/WarhousesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.WebAdmin.Controllers
+{
+    public class WarhousesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
