c6822f34b462b0dc946d4fc530aa50fc6a61a325
Vojtech Komurka <ramok@centrum.cz>
2022-09-12 10:21:36 +0200

    Add local Repository to SQLite library.

---
+++ Demo.SQLite/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+using Demo.Entities;
+
+namespace Demo.SQLite
+{
+    public interface IRepository<TEntity>
+        where TEntity : class, IEntity
+    {
+        Task AddAsync(TEntity entity);
+        Task AddAsync(IEnumerable<TEntity> entities);
+
+        Task Update(TEntity entity);
+        Task Update(IEnumerable<TEntity> entities);
+
+        Task DeleteAsync(Guid id);
+        Task Delete(TEntity entity);
+        Task Delete(IEnumerable<TEntity> entities);
+
+        Task<List<TEntity>> GetAsync();
+        Task<List<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+    }
+}
---
+++ Demo.SQLite/Repository.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq.Expressions;
+using Demo.Entities;
+using SQLite.Net.Async;
+
+namespace Demo.SQLite;
+
+public class Repository<TEntity> : IRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    private SQLiteAsyncConnection db;
+
+
+    public async Task AddAsync(TEntity entity)
+    {
+        await db.InsertAsync(entity);
+    }
+
+    public async Task AddAsync(IEnumerable<TEntity> entities)
+    {
+        await db.InsertAllAsync(entities);
+    }
+
+    public async Task Update(TEntity entity)
+    {
+        await db.UpdateAsync(entity);
+    }
+
+    public async Task Update(IEnumerable<TEntity> entities)
+    {
+        await db.UpdateAllAsync(entities);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var entity = await FirstOrDefaultAsync(c => c.Id == id);
+        await Delete(entity);
+    }
+
+    public async Task Delete(TEntity entity)
+    {
+        await db.DeleteAsync(entity);
+    }
+
+    public async Task Delete(IEnumerable<TEntity> entities)
+    {
+        foreach (var entity in entities)
+        {
+            await Delete(entity);
+        }
+    }
+
+    public Task<List<TEntity>> GetAsync()
+    {
+        return db.Table<TEntity>().ToListAsync();
+    }
+
+    public async Task<List<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        var query = db.Table<TEntity>();
+
+        if (predicate != null)
+        {
+            query = query.Where(predicate);
+        }
+        return await query.ToListAsync();
+    }
+
+    public async Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await db.FindAsync(predicate);
+    }
+}
