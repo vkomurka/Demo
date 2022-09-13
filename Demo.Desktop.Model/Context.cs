0b4e4cf9b87f75b51e61ca326e31775947ce49a6
Vojtech Komurka <ramok@centrum.cz>
2022-09-13 09:24:47 +0200

    Add Model project for demo desktop client.

---
+++ Demo.Desktop.Model/Context.cs
@@ -0,0 +1,10 @@
+﻿using Demo.Desktop.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Desktop.Model
+{
+    public class Context : DbContext
+    {
+        public DbSet<Setting> Settings { get; set; }
+    }
+}
---
+++ Demo.Desktop.Model/Entities/Setting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.DAL;
+
+namespace Demo.Desktop.Model.Entities
+{
+    public class Setting : IEntity
+    {
+        public Guid Id { get; set; }
+        public string  DemoWebServerUrl { get; set; }
+    }
+}
